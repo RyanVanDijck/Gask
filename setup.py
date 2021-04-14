4d947663859c5f6d565af417c56257540fc96288
rv17643 <rv17643@essex.ac.uk>
2021-04-14 17:13:02 +0100

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='Gask',
     version='0.3',
     packages=find_packages(),
-    #package_dir={'': 'gask'},
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     url='https://github.com/RyanVanDijck/Gask',
     license='GNU General Public License v3',
     author='ryan',
